c1f37fe86d094c81970992182c8e1747a5a922bb
/⁶⁶⁶Bunti <58895443+bntr00t@users.noreply.github.com>
2022-03-16 20:21:31 +0100

    improved
    faster and added colors

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os, httpx, time, threading
+from colorama import Fore
 from fake_useragent import UserAgent
 
 ua = UserAgent()
@@ -9,13 +10,13 @@ def xx(PROXY, url):
         try:
             req = client.get('https://1.1.1.1/')
             if req.status_code <= 400:
-                print ('[Valid] ' + PROXY + ' ' + str(req.status_code))
-                with open('valid.txt', 'a') as xX:
+                print (Fore.GREEN + '[Valid] ' + PROXY + ' ' + str(req.status_code))
+                with open('valid.txt', 'w') as xX:
                     xX.write(PROXY + '\n')
             else:
-                print('[Blocked] ' + PROXY + ' ' + str(req.status_code))
+                print(Fore.YELLOW + '[Blocked] ' + PROXY + ' ' + str(req.status_code))
         except httpx.HTTPError as exc:
-            print(exc)
+            print(Fore.RED + '[Bad] ' + PROXY) 
         return True
 
 def main():
@@ -27,11 +28,15 @@ def main():
     with open(fileproxy, 'r') as x:
         prox = x.read().splitlines()
     thread = []
+    print('starting threads')
+    time.sleep(1)
     for proxy in prox:
         t = threading.Thread(target=xx, args=(proxy, 'https://1.1.1.1/'))
         t.start()
         thread.append(t)
-        time.sleep(0.1)
+        time.sleep(0.05)
+    print('ending')
     for i in thread:
         i.join()
+    print('stopped ' + i + ' threads')
 main()
