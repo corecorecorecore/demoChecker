dc3b145a6896c25686d2e609b0950187e6373d32
/⁶⁶⁶Bunti <58895443+bntr00t@users.noreply.github.com>
2022-03-16 19:07:58 +0100

    update
    fully updated, faster and better now :D

--- main.py
+++ main.py
@@ -1,65 +1,37 @@
-import sys, httpx
-from threading import Thread
-from fake_headers import Headers
+import os, httpx, time, threading
+from fake_useragent import UserAgent
 
-proxiesFile = 'all.txt'
-
-
-aliveFile = 'alive.txt'
-aliveMode = 'w' # w=write, a=append
-
-proxyType = 'http' # type of proxies to check ('socks5' or 'http')
-
-proxylist = []
-workingProxies = []
-threads = []
-
-def import_proxies(proxiesFile):
-    print('[IMPORTING] Proxylist from ' + proxiesFile)
-    proxies = open(proxiesFile)
-    for x in proxies:
-        tmp = x.strip().split(":")
-        ip = tmp[0]
-        port = tmp[1]
-
-        proxy = {"ip": ip, "port": port}
-        proxylist.append(proxy)
-    proxies.close()
-    print('[IMPORTED] Proxylist')
-    return
-
-def check_proxy(proxy):
-    headers = Headers(headers=True).generate()
-    proxies = {'https://': proxyType + '://' + proxy['ip'] + ':' + proxy['port']}
+ua = UserAgent()
+def xx(PROXY, url):
+    proxies = {'https://': 'http://' + PROXY}
+    headers = {'User-Agent':str(ua.chrome)}
     with httpx.Client(http2=True,headers = headers, proxies=proxies) as client:
         try:
             req = client.get('https://1.1.1.1/')
             if req.status_code <= 400:
-                print('[Alive] ' + proxy['ip'] + ':' + proxy['port'] + ' - ' + str(req.status_code))
-                workingProxies.append(proxy)
-                alive = open('./'+aliveFile, aliveMode)
-                for proxy in workingProxies:
-                    alive.write(proxy['ip'] + ':' + proxy['port']+'\n')
-                alive.close()
+                print ('[Valid] ' + PROXY + str(req.status_code))
+                with open('valid.txt', 'a') as xX:
+                    xX.write(PROXY + '\n')
             else:
-                print('[Blocked] ' + proxy['ip'] + ':' + proxy['port'] + ' - ' + str(req.status_code))
+                print('[Blocked] ' + PROXY + str(req.status_code))
         except httpx.HTTPError as exc:
-            return exc
+            print(exc)
         return True
 
-def check_proxyList():
-    print('[CHECKING - Proxylist]')
-    for proxy in proxylist:
-        thread = Thread( target=check_proxy, args=(proxy, ))
-        thread.start()
-        threads.append(thread)
-
-    for thread in threads:
-        thread.join()
-    print('[DONE CHECKING - Proxylist]')
-    return
-
-
-import_proxies(proxiesFile)
-check_proxyList()
-print('[STATUS] ' + str(len(workingProxies)) + ' proxies from ' + str(len(proxylist)) + ' alive.')
+def main():
+    try:
+        fileproxy = input(' [+] File: ')
+    except:
+        print('  [-] Error : Enter Your Proxy List!')
+    os.system('cls' if os.name == 'nt' else 'clear')
+    with open(fileproxy, 'r') as x:
+        prox = x.read().splitlines()
+    thread = []
+    for proxy in prox:
+        t = threading.Thread(target=xx, args=(proxy, 'https://1.1.1.1/'))
+        t.start()
+        thread.append(t)
+        time.sleep(0.1)
+    for i in thread:
+        i.join()
+main()
