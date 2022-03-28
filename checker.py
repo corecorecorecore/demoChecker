95cbdd505f6753d7236f48f54fade0b2615cc47f
/⁶⁶⁶Bunti <58895443+bntr00t@users.noreply.github.com>
2022-03-28 19:28:59 +0200

    Update checker.py

--- checker.py
+++ checker.py
@@ -3,59 +3,10 @@ from colorama import Fore
 from fake_useragent import UserAgent
 from threading import Thread
 
-ua = UserAgent()
-headers_list = [
-{
-'User-Agent':str(ua.firefox),
-"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-"Accept-Language": "en-US,en;q=0.5",
-"Referer": "https://www.google.com/",
-"DNT": "1",
-"Connection": "keep-alive",
-"Upgrade-Insecure-Requests": "1"
-},
-{
-'User-Agent':str(ua.firefox),
-"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-"Accept-Language": "en-US,en;q=0.5",
-"Accept-Encoding": "gzip, deflate, br",
-"Referer": "https://www.google.com/",
-"DNT": "1",
-"Connection": "keep-alive",
-"Upgrade-Insecure-Requests": "1"
-},
-{
-"Connection": "keep-alive",
-"DNT": "1",
-"Upgrade-Insecure-Requests": "1",
-'User-Agent':str(ua.chrome),
-"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-"Sec-Fetch-Site": "none",
-"Sec-Fetch-Mode": "navigate",
-"Sec-Fetch-Dest": "document",
-"Referer": "https://www.google.com/",
-"Accept-Encoding": "gzip, deflate, br",
-"Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8"
-},
-{
-"Connection": "keep-alive",
-"Upgrade-Insecure-Requests": "1",
-'User-Agent':str(ua.chrome),
-"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-"Sec-Fetch-Site": "same-origin",
-"Sec-Fetch-Mode": "navigate",
-"Sec-Fetch-User": "?1",
-"Sec-Fetch-Dest": "document",
-"Referer": "https://www.google.com/",
-"Accept-Encoding": "gzip, deflate, br",
-"Accept-Language": "en-US,en;q=0.9"
-}
-]
-domain = 'https://1.1.1.1/'
+uafile = 'uas.txt'
+domain = 'https://1.1.1.1'
 def xx(PROXY,url):
-    proxies = {'https://': 'http://' + PROXY}
-    headers = random.choice(headers_list)
-    with httpx.Client(http2=True,headers = headers, proxies=proxies) as client:
+    with httpx.Client(http2=True,headers = {'user-agent':random.choice(list(map(lambda x:x.strip(),open(uafile)))),'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8','accept-encoding': 'gzip, deflate, br','accept-language': 'en'}, proxies={'https://': 'http://' + PROXY},follow_redirects=True) as client:
         try:
             req = client.get(url)
             if req.status_code <= 300:
@@ -86,7 +37,7 @@ def main():
         t = Thread(target=xx, args=(proxy,domain),daemon=True).start()
         time.sleep(0.01)
     print('ending')
-    time.sleep(10)
+    time.sleep(60)
     input('Exit')
 if __name__ == "__main__":
     main()
