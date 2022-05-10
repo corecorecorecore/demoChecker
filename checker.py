e52cc91aaac9756904ecda2d8550cbbb92ead574
/⁶⁶⁶Bunti <58895443+bntr00t@users.noreply.github.com>
2022-05-10 22:52:16 +0200

    update

--- checker.py
+++ checker.py
@@ -1,31 +1,57 @@
-import os, httpx, time, random, sys, ctypes
+import os, httpx, time, random, sys, ctypes, json
 from colorama import Fore
 from threading import Thread
 
-domain = 'https://1.1.1.1'
+domain = 'http://ip-api.com/json/'
 counter = [0,0,0,0,0]
+
+def correctSingleQuoteJSON(s):
+    rstr = ""
+    escaped = False
+
+    for c in s:
+    
+        if c == "'" and not escaped:
+            c = '"'
+        
+        elif c == "'" and escaped:
+            rstr = rstr[:-1]
+        
+        elif c == '"':
+            c = '\\' + c
+   
+        escaped = (c == "\\")
+        rstr += c
+    return rstr
+    
 def xx(PROXY,url):
-    try:
-        with httpx.Client(http2=True,headers = {'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8','accept-encoding': 'gzip, deflate','accept-language': 'en','connection': 'keep-alive','referer': domain,'Upgrade-Insecure-Requests': '1','user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:99.0) Gecko/20100101 Firefox/99.0'}, proxies={'all://': 'http://' + PROXY},follow_redirects=True) as client:
+        with httpx.Client(http2=True,headers = {'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:99.0) Gecko/20100101 Firefox/99.0','accept-language': 'en'},follow_redirects=True,proxies='http://' + PROXY) as client:
             try:
-                req = client.get(url)
-                if req.status_code <= 300:
-                    print (Fore.GREEN + '[Valid] ' + PROXY + ' ' + str(req.status_code))
-                    counter[1] = counter[1] + 1
-                    counter[4] = counter[4] + 1
-                    with open('http.txt', 'a') as xX:
-                        xX.write(PROXY + '\n')
-                else:
-                    print(Fore.YELLOW + '[Blocked] ' + PROXY + ' ' + str(req.status_code))
-                    counter[2] = counter[2] + 1
-                    counter[4] = counter[4] + 1
+                    req2 = client.get(url)
+                    correctJson = correctSingleQuoteJSON(str(req2.json()))
+                    m = json.loads(correctJson)
+                    if req2.status_code == 200 and 'success' in str(req2.content):
+                        print (Fore.GREEN + '[Valid] ' + PROXY + ' ' + str(req2.status_code) + ' ' + m["country"] + ' ' + m["as"])
+                        counter[1] = counter[1] + 1
+                        counter[4] = counter[4] + 1
+                        with open('http.txt', 'a') as xX:
+                            xX.write(PROXY + '\n')
+                    elif req2.status_code == 403:
+                        print (Fore.YELLOW + '[Blocked] ' + PROXY + ' ' + str(req2.status_code))
+                        counter[2] = counter[2] + 1
+                        counter[4] = counter[4] + 1
+                    else:
+                        print(Fore.RED + '[Bad] ' + PROXY + ' ' + str(req2.status_code))
+                        counter[3] = counter[3] + 1
+                        counter[4] = counter[4] + 1
             except httpx.HTTPError as exc:
                 counter[3] = counter[3] + 1
+                counter[4] = counter[4] + 1            
+            except:
+                counter[3] = counter[3] + 1
                 counter[4] = counter[4] + 1
-                #print(exc)
-                pass
-    except:
-        pass
+                #print(Fore.RED + '[Bad] ' + PROXY)
+                
 def main():
     if os.path.exists('http.txt'):
         os.remove('http.txt')
@@ -40,16 +66,23 @@ def main():
         prox = x.read().splitlines()
     print('Starting Threads')
     time.sleep(1)
+    Threads = []
     for proxy in prox:
-        t = Thread(target=xx, args=(proxy,domain),daemon=True).start()
+        t = Thread(target=xx, args=(proxy,domain),daemon=True)
+        t.start()
+        Threads.append(t)
         counter[0] = counter[0] + 1
         if os.name == 'nt':
             ctypes.windll.kernel32.SetConsoleTitleW('Proxies: ' + str(counter[4]) +'/'+ str(counter[0]) + ' | Valid: ' + str(counter[1]) + ' | Blocked: ' + str(counter[2]) + ' | Bad: ' + str(counter[3]))
         else:
             sys.stdout.write("\x1b]2;"+'Proxies: ' + str(counter[4]) +'/'+ str(counter[0]) + ' | Valid: ' + str(counter[1]) + ' | Blocked: ' + str(counter[2]) + ' | Bad: ' + str(counter[3])+"\x07")
         time.sleep(0.01)
-    print('ending')
-    time.sleep(60)
-    input('Exit')
+    for i in Threads:
+        i.join()
+        if os.name == 'nt':
+            ctypes.windll.kernel32.SetConsoleTitleW('Proxies: ' + str(counter[4]) +'/'+ str(counter[0]) + ' | Valid: ' + str(counter[1]) + ' | Blocked: ' + str(counter[2]) + ' | Bad: ' + str(counter[3]))
+        else:
+            sys.stdout.write("\x1b]2;"+'Proxies: ' + str(counter[4]) +'/'+ str(counter[0]) + ' | Valid: ' + str(counter[1]) + ' | Blocked: ' + str(counter[2]) + ' | Bad: ' + str(counter[3])+"\x07")
+        
 if __name__ == "__main__":
     main()
